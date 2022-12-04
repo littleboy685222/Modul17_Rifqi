ee0d1b575112e52b2f6e6d000d638b05277e9865
Muhammad Rifqi Fauzan <91310558+littleboy685222@users.noreply.github.com>
2022-12-04 22:23:07 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,17 +4,23 @@ const jwt = require('jsonwebtoken');
 SECRET = process.env.SECRET
 const Auth = {
     verifyToken(req, res, next){
-        const token = req.cookies['JWT'];
+        const tokenPM = req.cookies['JWT'];
+        const {token} = req.body;
         console.log(token);
         if (token) {
             // 12. Lakukan jwt verify
+            const decoded = jwt.verify(token, config.SECRET);
+            req.data = decoded;
+            return next()
+        } else if(tokenPM){
             try{
               const decoded = jwt.verify(token, SECRET);
               req.data = decoded;
             } catch(err){
               return res.status(401).send("TOKEN SALAH");
             } return next();
-        } else {
+        }
+            else {
           res.status(403).send({message: 'Youre not authenticated, please login first'})
             console.log('Youre not authenticated');
         }
